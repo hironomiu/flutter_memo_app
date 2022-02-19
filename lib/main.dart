f47075ca907df5b2cf2bbe99fffc91597f762689
hironomiu <hironomiu@gmail.com>
2022-02-20 08:31:28 +0900

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         // scaffoldBackgroundColor: Colors.green,
         primaryColor: Colors.blue,
       ),
